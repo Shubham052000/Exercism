edc54cf59ac715d60e42443d1b332002fe5ab507
Shubham Satyawali <shubham.satyawali.05@gmail.com>
2022-02-07 18:15:57 +0530

    3rd Problem solved

--- TypeScript/3_resistor-color-duo/resistor-color-duo.ts
+++ TypeScript/3_resistor-color-duo/resistor-color-duo.ts
@@ -1,3 +1,8 @@
-export function decodedValue() {
-  throw new Error('Remove this statement and implement this function')
+export function decodedValue(colorCodes: Array<string>): number {
+  const resistance_values: Array<string> = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white']
+  let resistance = "";
+  for(let i = 0; i<2; i++){
+    resistance += resistance_values.indexOf(colorCodes[i])
+  }
+  return Number(resistance)
 }
