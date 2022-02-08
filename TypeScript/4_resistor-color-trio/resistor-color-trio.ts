c98c0051b279490824ea79eac234bfb9bc6479cf
Shubham Satyawali <shubham.satyawali.05@gmail.com>
2022-02-09 00:39:05 +0530

    Another one bites the dust

--- TypeScript/4_resistor-color-trio/resistor-color-trio.ts
+++ TypeScript/4_resistor-color-trio/resistor-color-trio.ts
@@ -1,3 +1,22 @@
-export function decodedResistorValue() {
-  throw new Error('Remove this statement and implement this function')
+export function decodedResistorValue(arr: Array<string>): string {
+  const color_codes = [
+    "black",
+    "brown",
+    "red",
+    "orange",
+    "yellow",
+    "green",
+    "blue",
+    "violet",
+    "grey",
+    "white",
+  ];
+  let resistance = `${color_codes.indexOf(arr[0])}${color_codes.indexOf(
+    arr[1]
+  )}${"0".repeat(color_codes.indexOf(arr[2]))}`;
+  let res = '33000'
+if (resistance.slice(-3) === "000")
+    return resistance.slice(0, resistance.length - 3) + " kiloohms";
+  else return resistance + ' ohms';
+  
 }
