ea1181f685f6b351e276e1286cc2a03ce71dd784
Shubham Satyawali <shubham.satyawali.05@gmail.com>
2022-02-09 22:03:16 +0530

    6th Problem loaded

---
+++ TypeScript/6_resistor-color/resistor-color.ts
@@ -0,0 +1,5 @@
+export const colorCode = (inp: string): number => {
+  return COLORS.indexOf(inp);
+}
+
+export const COLORS = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white']
