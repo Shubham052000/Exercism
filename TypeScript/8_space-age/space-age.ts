dead21fbc301ef40c2377b5de56c85f9498fc586
Shubham Satyawali <shubham.satyawali.05@gmail.com>
2022-02-10 23:29:59 +0530

    My solution for 8th problem

--- TypeScript/8_space-age/space-age.ts
+++ TypeScript/8_space-age/space-age.ts
@@ -1,16 +1,16 @@
-const planetYearIRTEarth = {
-  earth: 1.0,
-  mercury: 0.2408467,
-  venus: 0.61519726,
-  mars: 1.8808158,
-  jupiter: 11.862615,
-  saturn: 29.447498,
-  uranus: 84.016846,
-  neptune: 164.79132
-};
-type Planet = keyof typeof planetYearIRTEarth;
+export function age(planet: string, seconds: number): number {
+  const planetYearIRTEarth = new Map([
+    ["mercury", 0.2408467],
+    ["venus", 0.61519726],
+    ["earth", 1.0],
+    ["mars", 1.8808158],
+    ["jupiter", 11.862615],
+    ["saturn", 29.447498],
+    ["uranus", 84.016846],
+    ["neptune", 164.79132],
+  ]);
 
-export function age(planet: Planet, seconds: number): number {
-  return parseFloat((seconds/(31557600*planetYearIRTEarth[planet])).toFixed(2))
+  return parseFloat(
+    (seconds / (31557600 * planetYearIRTEarth.get(planet)!)).toFixed(2)
+  );
 }
-
