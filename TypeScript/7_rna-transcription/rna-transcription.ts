c315d4beb12eeff7886ee1df8ecd4900fa4c989b
Shubham Satyawali <shubham.satyawali.05@gmail.com>
2022-02-10 23:12:17 +0530

    hmmm

--- TypeScript/7_rna-transcription/rna-transcription.ts
+++ TypeScript/7_rna-transcription/rna-transcription.ts
@@ -1,12 +1,16 @@
 export function toRna(dnaStrand: string): string {
-  const rnaComplementMap = new Map([['G', 'C'], ['C', 'G'], ['T', 'A'], ['A', 'U']]);
-  let rnaComplement = '';
-  for (let i = 0; i < dnaStrand.length; i++){
-    if(rnaComplementMap.has(dnaStrand[i]))
+  const rnaComplementMap = new Map([
+    ["G", "C"],
+    ["C", "G"],
+    ["T", "A"],
+    ["A", "U"],
+  ]);
+  let rnaComplement = "";
+  for (let i = 0; i < dnaStrand.length; i++) {
+    if (rnaComplementMap.has(dnaStrand[i]))
       rnaComplement += rnaComplementMap.get(dnaStrand[i]);
-    else
-      throw new Error('Invalid input DNA.')
+    else throw new Error("Invalid input DNA.");
   }
   return rnaComplement;
 }
-toRna('C');
+toRna("C");
