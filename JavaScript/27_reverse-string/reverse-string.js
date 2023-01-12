2e4a459efb359ab79ff4ae20625a152235b7ebd6
Shubham Satyawali <shubham.satyawali.05@gmail.com>
2023-01-12 20:12:23 +0530

    27th problem solved reverse string

---
+++ JavaScript/27_reverse-string/reverse-string.js
@@ -0,0 +1,10 @@
+//
+// This is only a SKELETON file for the 'Reverse String' exercise. It's been provided as a
+// convenience to get you started writing code faster.
+//
+
+export const reverseString = (string) => {
+  const stringLetters = string.split("");
+  const reverseArray = stringLetters.reverse();
+  return reverseArray.join("");
+};
